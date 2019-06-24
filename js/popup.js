8d05dac282422058f8a871a974b7c8fb94f4a255
AlbinaSam <alba91@inbox.ru>
2019-06-25 00:49:27 +0300

    Добавляет JS

---
+++ js/popup.js
@@ -0,0 +1,52 @@
+var link = document.querySelector(".address-card-button");
+var popup = document.querySelector(".modal-feedback");
+var close = popup.querySelector(".modal-close");
+var form = popup.querySelector(".feedback-form");
+var firstField = popup.querySelector("[name=name]");
+var secondField = popup.querySelector("[name=email]");
+var storage = localStorage.getItem("firstField");
+var isStorageSupport = true;
+var storage = "";
+
+try {
+    storage = localStorage.getItem("firstField");
+} catch (err) {
+    isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+
+    if (storage) {
+        firstField.value = storage;
+        secondField.focus();
+    } else {
+        firstField.focus();
+    }
+});
+
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!firstField.value || !secondField.value) {
+        evt.preventDefault();
+        popup.classList.add("modal-error");
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem("firstField", firstField.value);
+        }
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        popup.classList.remove("modal-show");
+    }
+});
